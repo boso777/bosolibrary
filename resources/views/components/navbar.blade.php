8ff6f91f4a52069fcb8f352e55e2a1008f50d04c
Boso <82522285+boso777@users.noreply.github.com>
2026-06-04 10:58:24 +0200

    fixed mobile navbar visualization

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -14,8 +14,10 @@
 
         {{-- Nav desktop --}}
         <nav class="hidden md:flex items-center gap-8 text-sm font-medium c-text-secondary font-primary">
-            <a class="hover:c-text-primary transition-colors" href="{{ route('books.index') }}">All Books</a>
-            <a class="hover:c-text-primary transition-colors" href="@auth {{ route('books.create') }} @endauth @guest {{ route('login') }} @endguest">Add Book</a>
+            @auth <a class=" transition-colors" href="{{ route('books.index') }}">All Books</a> @endauth
+            <a class=" transition-colors" href="@auth {{ route('books.create') }} @endauth @guest {{ route('login') }} @endguest">Add Book</a>
+            <a class=" transition-colors" href="{{ route('books.index') }}">About us</a>
+            <a class=" transition-colors" href="{{ route('books.index') }}">Coffee time</a>
         </nav>
 
         {{-- CTA desktop + hamburger mobile --}}
@@ -38,7 +40,7 @@
             @endauth
 
             {{-- Hamburger --}}
-            <button id="openMenu" class="md:hidden c-text-secondary hover:c-text-primary transition-colors">
+            <button id="openMenu" class="md:hidden c-text-secondary  transition-colors">
                 <svg class="w-6 h-6" fill="none" stroke="currentColor" stroke-width="2"
                      viewBox="0 0 24 24" stroke-linecap="round" stroke-linejoin="round">
                     <path d="M4 6h16M4 12h16M4 18h16"/>
@@ -50,12 +52,14 @@
     {{-- Menu mobile --}}
     <div id="mobileMenu"
          class="hidden md:hidden max-w-4xl mx-auto mt-2 c-bg-dark border border-gray-800 rounded-2xl shadow-xl px-6 py-4 flex flex-col gap-4 text-sm c-text-secondary font-primary">
-        <a class="hover:c-text-primary custom-btn" href="{{ route('books.index') }}">All Books</a>
-        <a class="hover:c-text-primary custom-btn" href="@auth {{ route('books.create') }} @endauth @guest {{ route('login') }} @endguest">Add Book</a>
+        @auth <a class="block py-2" href="{{ route('books.index') }}">All Books</a> @endauth
+        <a class="block py-2" href="@auth {{ route('books.create') }} @endauth @guest {{ route('login') }} @endguest">Add Book</a>
+        <a class="block py-2" href="#">About us</a>
+        <a class="block py-2" href="#">Coffee time</a>
 
         @guest
             <a href="{{ route('login') }}"
-               class="inline-flex justify-center c-bg-primary c-text-secondary px-5 py-2 rounded-full font-semibold hover:opacity-90 transition">
+               class="custom-btn">
                 Sign In
             </a>
         @endguest
